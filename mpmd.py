359d6b9d5eecf70a41d2ec166bc99e8b90d24f3b
C Anthony Risinger <c@anthonyrisinger.com>
2019-01-10 22:56:51 -0600

    Change safe height to 10 instead of 15.

--- mpmd.py
+++ mpmd.py
@@ -245,7 +245,7 @@ class Printer:
         self.G1(**kwds)
         return self
 
-    def bed(self, I=3, J=3, X=0, Y=0, Z=15):
+    def bed(self, I=3, J=3, X=0, Y=0, Z=10):
         """Compute XYZ coords for IJ bed mesh index."""
         assert I >= 0 and I <= 6
         assert J >= 0 and J <= 6
